24a0dd5e568fefee39ecab4bf4f5fa0c1023936e
lino-dextra <lino.veloso@dextra-sw.com>
2020-01-22 14:55:29 -0300

    ttreinamento de html_css_js

---
+++ html_css_js/treinamento-stags/src/script.js
@@ -0,0 +1,37 @@
+loadPokemons().then(loadEvolutions).then(() => {
+    var items = document.getElementsByClassName("evolutions__item")
+    items = Array.from(items)
+
+    items.forEach((element, index )=> {
+        var children = element.children
+        children = Array.from(children)
+
+        children[0].src= pokemons[index].sprites.front_default  
+        children[1].src= pokemons[index].sprites.front_default
+        children[2].src= pokemons[index].sprites.front_default
+
+        
+    });
+});
+
+async function loadEvolutions() {
+    evolutions = [];
+    for (let i = 1; i <= 10; i++) {
+        chain = [];
+        const x = await fetch(`https://pokeapi.co/api/v2/evolution-chain/${i}`)
+        let y = await x.json();
+        chain_step = y.chain
+
+        while (chain_step.evolves_to.length > 0) {
+            actualPokemon = pokemons.find(item => item.name === chain_step.species.name)
+
+            actualPokemon ? chain.push(actualPokemon) : null;
+
+            chain_step = chain_step.evolves_to[0]
+        }
+        actualPokemon = pokemons.find(item => item.name === chain_step.species.name)
+        actualPokemon ? chain.push(actualPokemon) : null;
+
+        evolutions.push(chain)
+    }
+}
